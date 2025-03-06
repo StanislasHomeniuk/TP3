4b34acce7cee0fa504dcbaa4d976b1f9cfe18ad2
StanislasHomeniuk <stanislas.homeniuk@etu.univ-orleans.fr>
2025-03-06 08:35:29 +0100

    ajout méthode add

--- Box.java
+++ Box.java
@@ -1,3 +1,13 @@
-public class Box{
-public Box(){System.out.println("Box créée");}
+import java.util.ArrayList;
+
+public class Box {
+   ArrayList<String> contents = new ArrayList<String>();
+
+   public Box() {
+      System.out.println("Box créée");
+   }
+
+   public void add(String truc) {
+      this.contents.add(truc);
+   }
 }
