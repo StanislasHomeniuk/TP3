ee024fa4b6beb988797bc35668c4a65ff915afbc
StanislasHomeniuk <stanislas.homeniuk@etu.univ-orleans.fr>
2025-03-06 09:05:34 +0100

    ajout test add

---
+++ TestsBoxes.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+public class TestsBoxes {
+    @Test
+    public void testBoxCreate() {
+    Box b = new Box();
+    }
+/** on veut pouvoir mettre des trucs dedans */
+@Test
+public void testBoxAdd(){
+    Box b = new Box();
+    b.add("truc1");
+    b.add("truc2");
+    }
+}
