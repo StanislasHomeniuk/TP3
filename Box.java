fe2ea2a4a3bc35ed201166e0b1a8b9cb0733d1eb
StanislasHomeniuk <stanislas.homeniuk@etu.univ-orleans.fr>
2025-03-06 08:18:35 +0100

    Biblio Box: ajout de test_box_add

--- Box.java
+++ Box.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+
 public class Box{
-public Box(){System.out.println("Box créée");}
+    ArrayList<String> contents = new ArrayList<String>();
+    public Box(){System.out.println("Box créée");}
+
+    public void add(String truc){
+        this.contents.add(truc);
+    }
 }
--- TestsBoxes.java
+++ TestsBoxes.java
@@ -2,8 +2,16 @@ import org.junit.*;
 import static org.junit.Assert.assertEquals;
 
 public class TestsBoxes{
-@Test
-public void testBoxCreate(){
-Box b = new Box();
-}
+    @Test
+    public void testBoxCreate(){
+    Box b = new Box();
+    }
+    /** on veut pouvoir mettre des trucs dedans */
+
+    @Test
+    public void testBoxAdd(){
+        Box b = new Box();
+        b.add("truc 1");
+        b.add("truc 2");
+    }
 };
